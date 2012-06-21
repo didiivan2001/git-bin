5e8162483da7de236af7631f379eede072ecc2e4
Shmarya <srubenstein@nds.com>
2012-06-21 16:01:27 +0300

    fixed messages, use real binstore path, check binstore exists

--- git-bin.sh
+++ git-bin.sh
@@ -1,6 +1,9 @@
-BINSTORE=~/tmp/gitbin/binstore
+remotename=`git remote -v | grep -E "origin.*push" | grep -oE ":([^ ]+)" | grep -oE "[^:]+"`
+BINSTORE=$(dirname /media/lab/gitserve/gitbin-store/$remotename/blah) #using dirname with a fake file to clean up double slashes etc.
 
 function addbin {
+    [[ -d $BINSTORE ]] || mkdir -p $BINSTORE
+
     while [[ "x$1" != "x" ]];
     do
         src=$1 ; shift
@@ -9,25 +12,31 @@ function addbin {
         # i.e. there is nothing to reset
         [[ -L $src && "`readlink $src`" == "$BINSTORE/$hash" ]] && continue 
 
-        echo "addbin: adding $src with hash $hash to $BINSTORE"
+        echo "git-bin-add: adding $src with hash $hash to $BINSTORE"
 
         if [[ -e $BINSTORE/$hash && `stat -c%s $BINSTORE/$hash` != `stat -c%s $src` ]]; then
-            echo "addbin: SIGNATURE CONFLICT in $src!"
-            echo "addbin: $src  in store has size `stat -c%s $BINSTORE/$hash`, and your file has size `stat -c%s $src`"
+            echo "git-bin-add: SIGNATURE CONFLICT in $src!"
+            echo "git-bin-add: $src  in store has size `stat -c%s $BINSTORE/$hash`, and your file has size `stat -c%s $src`"
             return
         elif [[ -e $BINSTORE/$hash && -h $src ]]; then
-            echo "addbin: nothing to do, $src is already in the binstore"
+            echo "git-bin-add: nothing to do, $src is already in the binstore"
             return
         fi
 
         # BACKUP
         cp -f $src .tmp_$hash
 
-        (cp -f $src $BINSTORE/$hash && rm -f $src && ln -s $BINSTORE/$hash $src && git add $src && rm -f .tmp_$hash) || (echo "addbin: something went wrong when adding $src, reverting" && mv -f .tmp_$hash $src)
+        (cp -f $src $BINSTORE/$hash && rm -f $src && ln -s $BINSTORE/$hash $src && git add $src && rm -f .tmp_$hash) || (echo "git-bin-add: something went wrong when adding $src, reverting" && mv -f .tmp_$hash $src)
     done
 }
 
 function editbin {
+    if [[ ! -d $BINSTORE ]];
+    then
+        echo "git-bin-edit: the binstore ($BINSTORE) doesn't exist. You probably haven't added any binaries to git-bin yet."
+        return
+    fi
+
     while [[ "x$1" != "x" ]];
     do
         src=$1 ; shift
@@ -35,11 +44,17 @@ function editbin {
         [[ $? == 1 || "$(dirname $storefile)" != "$BINSTORE" ]] && continue #not a symlink
         tmpfile=.tmp_$(basename $storefile)
 
-        cp $storefile $tmpfile && mv -f $tmpfile $src && echo "editbin: $src is now available for editing"
+        cp $storefile $tmpfile && mv -f $tmpfile $src && echo "git-bin-edit: $src is now available for editing"
     done
 }
 
 function resetbin {
+    if [[ ! -d $BINSTORE ]];
+    then
+        echo "git-bin-reset: the binstore ($BINSTORE) doesn't exist. You probably haven't added any binaries to git-bin yet."
+        return
+    fi
+
     while [[ "x$1" != "x" ]];
     do
         src=$1 ; shift
@@ -60,18 +75,18 @@ function resetbin {
             git status $src | grep -E "typechange: $src\$" 2>&1 >/dev/null || continue
             
             #otherwise, the file has changed, so we should back it up!
-            echo "resetbin: $src has changed, saving a copy to /tmp/$src.$hash"
+            echo "git-bin-reset: $src has changed, saving a copy to /tmp/$src.$hash"
             cp -f $src /tmp/$src.$hash
         else
             # The has is in the binstore. We need to check for signature conflicts:
             if [[ `stat -c%s $BINSTORE/$hash` != `stat -c%s $src` ]]; then
-                echo "resetbin: SIGNATURE CONFLICT in $src!"
-                echo "resetbin: $src in store has size `stat -c%s $BINSTORE/$hash`, and your file has size `stat -c%s $src`"
+                echo "git-bin-reset: SIGNATURE CONFLICT in $src!"
+                echo "git-bin-reset: $src in store has size `stat -c%s $BINSTORE/$hash`, and your file has size `stat -c%s $src`"
                 return
             fi
         fi
         # now we can just restore the file using git.
-        echo "resetbin: restoring $src to the git HEAD"
+        echo "git-bin-reset: restoring $src to the git HEAD"
         rm -f $src && git checkout -- $src
     done
 }
@@ -82,7 +97,7 @@ case $funcname in
     add|edit|reset) 
         if [[ "x$1" == "x" ]];
         then
-            echo "git bin: You must specify a file name to operate on!"
+            echo "git-bin: you must specify a file name to operate on!"
             exit 1
         fi
         eval ${funcname}bin $@
