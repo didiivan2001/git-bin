2a3c018bbf2889265e2d1ea273c65f08ecfa9fea
srubenst <srubenst@cisco.com>
2014-05-09 13:37:44 +0300

    Added git repo find and init functions to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import platform
 import subprocess
+import dulwich.repo
+import os.path
+import os
 
 
 def call_prog(cmd, args):
@@ -22,3 +25,26 @@ elif platform.system() == "Linux":
     def md5_file(filename):
         res = call_prog("md5sum", ["--tag", filename])
         return res.rsplit("=")[1].strip()
+
+
+def find_repo_root(path):
+    if path.endswith(os.sep):
+        path = path[:-1]
+
+    while path not in ["", None, os.sep]:
+        if os.path.exists(os.path.join(path, ".git")):
+            break
+        path, null = os.path.split(path)
+    if path in ["", None, os.sep]:
+        return None
+    return path
+
+
+def get_repo(path=None):
+    if not path:
+        path = os.getcwd()
+    root = find_repo_root(path)
+    if root:
+        return dulwich.repo.Repo(root)
+    return None
+
