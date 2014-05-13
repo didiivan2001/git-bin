94a3592680264ac63b1d43eab7067c924d6d492b
srubenst <srubenst@cisco.com>
2014-05-13 18:26:33 +0300

    split commands out into their own file

---
+++ commands.py
@@ -0,0 +1,144 @@
+import sys
+import os
+import shutil
+
+
+class Command(object):
+
+    def __init__(self):
+        pass
+
+    def execute(self, *args, **kwargs):
+        raise NotImplemented()
+
+
+class UndoableCommand(Command):
+
+    def __init__(self):
+        Command.__init__(self)
+
+    def execute(self):
+        try:
+            return self._execute()
+        except Exception:
+            self.undo()
+            exc_info = sys.exc_info()
+            raise exc_info[1], None, exc_info[2]
+
+    def _execute(self):
+        raise NotImplemented()
+
+    def undo(self):
+        raise NotImplemented()
+
+
+class CompoundCommand(Command):
+    # TODO: this should really be an UndoableCommand
+    # TODO: keep track of which commands completed successfully so that we can undo them if needs be
+
+    def __init__(self, *args):
+        self.commands = args
+
+    def execute(self):
+        for cmd in self.commands:
+            cmd.execute()
+
+    def push(self, command):
+        self.commands.append(command)
+
+
+class NotAFileException(BaseException):
+    pass
+
+
+class CopyFileCommand(Command):
+
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+        if not os.path.isfile(src):
+            raise NotAFileException()
+
+    def _execute(self):
+        shutil.copy(self.src, self.dest)
+
+
+class MoveFileCommand(UndoableCommand):
+
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+        if not os.path.isfile(src):
+            raise NotAFileException()
+
+    def _execute(self):
+        # TODO: check for existance of dest and maybe abort? As it is, this
+        # will automatically overwrite.
+        shutil.move(self.src, self.dest)
+
+    def undo(self):
+        # TODO: perhaps check to see that the file was moved cleanly?
+        shutil.move(self.dest, self.src)
+
+
+class SafeMoveFileCommand(MoveFileCommand):
+
+    def __init__(self, src, dest, backupfile_dir=None):
+        MoveFileCommand.__init__(self, src, dest)
+        if not backupfile_dir:
+            self.backupfile_dir = os.path.dirname(self.src)
+        else:
+            self.backupfile_dir = backupfile_dir
+
+    def _execute(self):
+        backup_filename = os.path.join(self.backupfile_dir, "._tmp_." + os.path.basename(self.src))
+        # keep a copy of the files locally
+        self.copy_cmd = CopyFileCommand(self.src, backup_filename)
+        self.copy_cmd.execute()
+        # move the file
+        MoveFileCommand._execute(self)
+        # remove the local copy
+        os.remove(backup_filename)
+
+    def undo(self):
+        backup_filename = os.path.join(self.backupfile_dir, "._tmp_." + os.path.basename(self.src))
+        shutil.move(backup_filename, self.src)
+
+
+class LinkToFileCommand(Command):
+
+    def __init__(self, linkname, targetname):
+        self.linkname = linkname
+        self.targetname = targetname
+
+    def _execute(self):
+        os.symlink(self.targetname, self.linkname)
+
+
+class ChmodCommand(UndoableCommand):
+
+    def __init__(self, modes, filename):
+        self.modes = modes
+        self.filename = filename
+
+    def _execute(self):
+        self.previous_modes = os.stat(self.filename).S_IMODE
+        os.chmod(self.filename, self.modes)
+
+    def undo(self):
+        # TODO: it's conceivable that the modes were set and the user no longer
+        # has permission to modify the modes. Not sure what to do in that case.
+        os.chmod(self.filename, self.previous_modes)
+
+
+class GitAddCommand(UndoableCommand):
+
+    def __init__(self, gitrepo, filename):
+        self.gitrepo = gitrepo
+        self.filename = filename
+
+    def _execute(self):
+        self.gitrepo.index.add(self.filename)
+
+    def undo(self):
+        self.gitrepo.index.remove(self.filename)
--- git-bin.py
+++ git-bin.py
@@ -1,120 +1,11 @@
 #!/usr/bin/env python
 import os.path
-import glob
 import argparse
+import stat
+
 import utils
-import shutil
-import sys
-
-
-class Command(object):
-
-    def __init__(self):
-        pass
-
-    def execute(self, *args, **kwargs):
-        raise NotImplemented()
-
-
-class UndoableCommand(Command):
-
-    def __init__(self):
-        Command.__init__(self)
-
-    def execute(self):
-        try:
-            return self._execute()
-        except Exception:
-            self.undo()
-            exc_info = sys.exc_info()
-            raise exc_info[1], None, exc_info[2]
-
-    def _execute(self):
-        raise NotImplemented()
-
-    def undo(self):
-        raise NotImplemented()
-
-
-class CompoundCommand(Command):
-
-    def __init__(self, *args):
-        self.commands = args
-
-    def execute(self):
-        for cmd in self.commands:
-            cmd.execute()
-
-    def push(self, command):
-        self.commands.append(command)
-
-
-class NotAFileException(BaseException):
-    pass
-
-
-class CopyFileCommand(Command):
-
-    def __init__(self, src, dest):
-        self.src = src
-        self.dest = dest
-        if not os.path.isfile(src):
-            raise NotAFileException()
-
-    def _execute(self):
-        shutil.copy(self.src, self.dest)
-
-
-class MoveFileCommand(UndoableCommand):
-
-    def __init__(self, src, dest):
-        self.src = src
-        self.dest = dest
-        if not os.path.isfile(src):
-            raise NotAFileException()
-
-    def _execute(self):
-        # TODO: check for existance of dest and maybe abort? As it is, this
-        # will automatically overwrite.
-        shutil.move(self.src, self.dest)
-
-    def undo(self):
-        # TODO: perhaps check to see that the file was moved cleanly?
-        shutil.move(self.dest, self.src)
-
-
-class SafeMoveFileCommand(MoveFileCommand):
-
-    def __init__(self, src, dest, backupfile_dir=None):
-        MoveFileCommand.__init__(self, src, dest)
-        if not backupfile_dir:
-            self.backupfile_dir = os.path.dirname(self.src)
-        else:
-            self.backupfile_dir = backupfile_dir
-
-    def _execute(self):
-        backup_filename = os.path.join(self.backupfile_dir, "._tmp_." + os.path.basename(self.src))
-        # keep a copy of the files locally
-        self.copy_cmd = CopyFileCommand(self.src, backup_filename)
-        self.copy_cmd.execute()
-        # move the file
-        MoveFileCommand._execute(self)
-        # remove the local copy
-        os.remove(backup_filename)
-
-    def undo(self):
-        backup_filename = os.path.join(self.backupfile_dir, "._tmp_." + os.path.basename(self.src))
-        shutil.move(backup_filename, self.src)
-
-
-class LinkToFileCommand(Command):
-
-    def __init__(self, linkname, targetname):
-        self.linkname = linkname
-        self.targetname = targetname
-
-    def _execute(self):
-        os.symlink(self.targetname, self.linkname)
+import git
+import commands as cmd
 
 
 class Binstore(object):
@@ -122,25 +13,54 @@ class Binstore(object):
     def __init__(self):
         pass
 
-    def add_file(self, filename):
+    def init(self):
+        """ Initialize git-bin for this git repository."""
         raise NotImplemented
 
+    def add_file(self, filename):
+        """ Add the specified file to the binstore. """
+        raise NotImplemented
+
+    def edit_file(self, filename):
+        """ Retrieve the specified file for editing. """
+        raise NotImplemented
+
+    def reset_file(self, filename):
+        """ Reset the specified file. """
+
+    def __contains__(self, item):
+        """ Test whether a given item is in this binstore. The item may be a hash or a symlink in the repo """
+        raise NotImplemented
 
 
 class FilesystemBinstore(Binstore):
 
-    def __init__(self, path):
+    def __init__(self, gitrepo, binstore_base):
         Binstore.__init__(self)
-        self.path = path
+        self.gitrepo = gitrepo
+        # retrieve the binstore path from the .git/config
+        # TODO: what should we do if there is no path? It's a git-bin-init situation
+        self.path = gitrepo.config.get("binstore", "path")
+        if not self.path:
+            self.init()
+
+    def init(self):
+        # TODO: create the ${binstore_base}/${repo} directory
+        # TODO: create the .git/binstore symlink
+        # TODO: set the settings in .git/config [binstore]
+        # TODO: set self.path
+        raise NotImplemented
 
     def add_file(self, filename):
         digest = utils.md5_file(filename)
         binstore_filename = os.path.join(self.path, digest)
 
-        commands = CompoundCommand(
+        commands = cmd.CompoundCommand(
             [
-                SafeMoveFileCommand(filename, binstore_filename),
-                LinkToFileCommand(filename, binstore_filename)
+                cmd.SafeMoveFileCommand(filename, binstore_filename),
+                cmd.LinkToFileCommand(filename, binstore_filename),
+                cmd.ChmodCommand(stat.S_IRUSR, stat.S_IRGRP, stat.S_IROTH, binstore_filename),
+                cmd.GitAddCommand(self.gitrepo, filename),
             ]
         )
 
@@ -149,9 +69,9 @@ class FilesystemBinstore(Binstore):
     def edit_file(self, filename):
         binstore_filename = os.readlink(filename)
 
-        commands = CompoundCommand(
+        commands = cmd.CompoundCommand(
             [
-                SafeMoveFileCommand(binstore_filename, filename, os.path.dirname(filename)),
+                cmd.SafeMoveFileCommand(binstore_filename, filename, os.path.dirname(filename)),
             ]
         )
 
@@ -174,6 +94,7 @@ class GitBin(object):
         self.binstore = binstore
 
     def add(self, filenames):
+        # TODO: resolve globs, probably in the caller
         """ Add a list of files, specified by their full paths, to the binstore. """
         if not isinstance(filenames, list):
             filenames = [filenames]
@@ -198,126 +119,21 @@ class GitBin(object):
             # TODO: maybe make recursive directory crawls optional/configurable
             if os.path.isdir(filename):
                 for root, dirs, files in os.walk(filename):
+                    # first add all directories recursively
+                    self.add([os.path.join(root, dn) for dn in dirs])
+                    # now add all the files
                     self.add([os.path.join(root, fn) for fn in files])
+                return
 
-
-class AddCommand(Command):
-
-    def __init__(self, binstore):
-        Command.__init__(self, binstore)
-
-    def _perform(self, files):
-        for f in files:
-            fi = FileInfo(f)
-
-            # if we're looking at the current dir, skip
-            if fi.basename() == ".":
-                continue
-
-            # if the target file is a real symlink, i.e. one which doesn't point
-            # into the binstore, just add it as is. This needs to be done before
-            # the test for directory, so that we don't follow symlinked dirs.
-            if fi.is_symlink() and fi.is_symlink_to_binstore():
-                do_git_add
-                continue
-
-            # if the target file is a directory, we should add recursively.
-            if fi.is_dir():
-                self._print("descending into %s for recursive add." % f)
-                self._perform(fi.get_children("*"))
-
-            # if the target file is not a binary file, just add it
-            if not fi.is_binary():
-                do_git_add
-                continue
-
-            hash = fi.get_hash()
-
-            # if the file hash already exists in the binstore, and the target file
-            # is a symlink to it, there's nothing to do.
-            if fi.points_to_binstore():
-                continue
-
-            self._print("adding %s with hash %s to binstore (%s)" % (f, hash, self.binstore))
-
-            if self.binstore.has_file(hash):
-                if self.binstore.get_fileinfo(hash).size() != fi.size():
-                    # CONFLICT: same signature, different file size
-                    raise Exception("Conflict")
-                else:
-                    # DUPLICATE: there were multiple copies of the same file in
-                    # the original binary files.
-                    self.binstore.set_link(fi, hash)
-
-            self.binstore.add(fi, hash)
-
-
-class EditCommand(Command):
-
-    def __init__(self, binstore):
-        Command.__init__(self, binstore)
-
-    def _perform(self, files):
-        for f in files:
-            fi = FileInfo(f)
-
-            # if the file is a directory, skip it as we don't allow recursive
-            # edits
-            if fi.is_dir():
-                self._print("recursive edits are disabled for your safety and sanity")
-                continue
-
-            if fi.points_to_binstore(self.binstore):
-                self.binstore.get_file(fi)
-
-
-class ResetCommand(Command):
-
-    def __init__(self, binstore):
-        Command.__init__(self, binstore)
-
-    def _perform(self, files):
-        for f in files:
-            fi = FileInfo(f)
-
-            # if the file is a directory, skip it as we don't allow recursive
-            # resets
-            if fi.is_dir():
-                self._print("recursive resets are disabled for your safety and sanity")
-                continue
-
-            status = fi.git_status()
-
-            # if the file is not binary, just use the standard git operations
-            if not fi.is_binary():
-                do_git_reset
-
-            if status == "A":
-                # newly added file, not yet committed. Need to copy the actual
-                # contents back and do a git reset HEAD
-                pass
-            elif status == "T":
-                # type-change means we did a git bin edit (type changed from
-                # symlink to real file). We can just do a git checkout --
-                # if the file contents have changed, we should make a local backup
-                pass
-            else:
-                # uh oh.
-                raise Exception()
-
-
-commands = {
-    "add": AddCommand,
-    "edit": EditCommand,
-    "reset": ResetCommand,
-}
+            # at this point, we're only dealing with a file, so let's add it to the binstore
+            self.binstore.add_file(filename)
 
 
 def build_options_parser():
     parser = argparse.ArgumentParser(description='git bin')
     parser.add_argument(
         'command',
-        choices=sorted(commands.keys()),
+        choices=["add", "edit", "reset"],
         help='the command to perform')
     parser.add_argument(
         '-v', '--verbose',
@@ -347,8 +163,6 @@ def build_options_parser():
 # - detect online binstore available. if so, and was offline, suggest going online.
 def main(args):
     binstore = Binstore()
-    if args.command in commands:
-        commands[args.command](binstore)._perform(args.files)
 
 if __name__ == '__main__':
     args = build_options_parser().parse_args()
