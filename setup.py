97d0bc5d39b13a996b8b648fd0c373a91eeb3c3c
srubenst <srubenst@cisco.com>
2014-07-08 12:15:05 +0300

    working setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='gitbin',
+    version='0.2.0',
+    description='git extension to support binary files',
+    author='SAS team',
+    author_email='stare-c-sas@cisco.com',
+    url='https://git-sas.cisco.com/tools/gitbin.git',
+    download_url='https://git-sas.cisco.com/tools/gitbin.git',
+    packages=["gitbin"],
+    install_requires=['sh'],
+    entry_points={
+        'console_scripts': [
+            'git-bin = gitbin.gitbin:main'
+        ]
+    },
+)
