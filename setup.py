ba0f4e4736495a0dca96560fccdc164a9f0c7302
srubenst <srubenst@cisco.com>
2015-05-12 11:33:52 -0400

    Roll back the name change from git-bin to gitbin because python fails at dashes

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 
 setup(
-    name='git-bin',
+    name='gitbin',
     version='0.3.0',
     description='git extension to support binary files',
     author='SAS team',
     author_email='stare-c-sas@cisco.com',
     url='https://offensive-git.cisco.com/sas-tools/git-bin.git',
     download_url='https://offensive-git.cisco.com/sas-tools/git-bin.git',
-    packages=["git-bin"],
+    packages=["gitbin"],
     install_requires=['sh', 'docopt'],
     entry_points={
         'console_scripts': [
-            'git-bin = git-bin.git-bin:main'
+            'git-bin = gitbin.gitbin:main'
         ]
     },
 )
