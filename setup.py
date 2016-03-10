6e2a83a81821f517c03f8173464bdd4e4fa07064
srubenst <srubenst@cisco.com>
2016-03-10 17:17:28 +0200

    Fix urls in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description='git extension to support binary files',
     author='srubenst',
     author_email='srubenst@cisco.com',
-    url='https://github.com/cisco/git-bin.git',
-    download_url='https://github.com/cisco-sas/git-bin.git',
+    url='https://github.com/cisco-sas/git-bin',
+    download_url='https://github.com/cisco-sas/git-bin',
     packages=["gitbin"],
     install_requires=['sh', 'docopt'],
     entry_points={
